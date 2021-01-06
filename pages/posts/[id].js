dcd3f679e83cf1b1ee22a4bc97cdc7eea35995b2
dahee kim <ekekekgml@naver.com>
2021-01-06 20:05:12 +0900

    Add Dynamic Routes chapter code

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
+
+export default function Post({postData}){
+    return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+    )
+}
+
+//returns an array of possible values for id
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+      paths,
+      //if fallback is 'false', then any paths not returned by 'getStaticPaths' wil result in a 404page
+      //if fallback is 'true', then the behavior of 'getStaticProps' changes
+      fallback: false
+    }
+  }
+  
+//fetches necessary data for the post with id
+  export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+      props: {
+        postData
+      }
+    }
+  }
